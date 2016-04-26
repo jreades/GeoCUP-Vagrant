c0c491d8e2b5e60967293f79e1adee21b806376e
Jon Reades <jon@reades.com>
2016-04-26 15:20:15 +0100

    Adjusting to deal with particularities of GeoCUP
    I have my own ‘bootstrap’ script, so this avoids any confusion about
    which is being used.

--- vagrant/bootstrap-vagrant.sh
+++ vagrant/bootstrap-vagrant.sh
@@ -12,7 +12,7 @@ export PATH=/home/vagrant/miniconda/bin:$PATH
 echo "export PATH=/home/vagrant/miniconda/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 
-#wget http://darribas.org/gds15/content/infrastructure/install_gds_stack_unix.sh -O /vagrant/install_gds_stack_unix.sh
-chmod +x /vagrant/install_gds_stack_unix.sh
-sh /vagrant/install_gds_stack_unix.sh
+wget http://darribas.org/gds15/content/infrastructure/install_gds_stack_unix.sh -O install_gds_stack_unix.sh
+chmod +x install_gds_stack_unix.sh
+./install_gds_stack_unix.sh
 
