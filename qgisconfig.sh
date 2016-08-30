448d15fc87aa3adfdc2ce15f982f76ffebe3438d
Jon Reades <jon@reades.com>
2016-08-30 10:07:42 +0100

    Updating GPG key stuff

--- qgisconfig.sh
+++ qgisconfig.sh
@@ -4,14 +4,15 @@
 # QGIS libs so that we're not dependent
 # on the really old versions in the main repos
 sudo sh -c 'echo "# QGIS Repositories" >> /etc/apt/sources.list'
-sudo sh -c 'echo "deb http://qgis.org/ubuntugis trusty main" >> /etc/apt/sources.list'  
-sudo sh -c 'echo "deb-src http://qgis.org/ubuntugis trusty main" >> /etc/apt/sources.list'  
+sudo sh -c 'echo "deb http://qgis.org/ubuntugis xenial main" >> /etc/apt/sources.list'  
+sudo sh -c 'echo "deb-src http://qgis.org/ubuntugis xenial main" >> /etc/apt/sources.list'  
 
 # Add the QGIS signing key so that apt-get
 # doesn't complain about missing public keys
-wget -O - http://qgis.org/downloads/qgis-2015.gpg.key | gpg --import
-gpg --fingerprint 3FF5FFCAD71472C4
-gpg --export --armor 3FF5FFCAD71472C4 | sudo apt-key add -
+# Note that these expire: gpg-2016 [expires: 2017-08-17]
+wget -O - http://qgis.org/downloads/qgis-2016.gpg.key | gpg --import
+gpg --fingerprint 073D307A618E5811
+gpg --export --armor 073D307A618E5811 | sudo apt-key add -
 
 # And update the repos and install QGIS
 sudo apt-get update 
