e2440e6bddd5dc8999dbb22b53d0d43faa078ee5
Jon Reades <jon@reades.com>
2017-02-09 18:40:30 +0000

    Think I have this working now...

--- vagrant/bootstrap-vagrant.sh
+++ vagrant/bootstrap-vagrant.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get dist-upgrade -y
+sudo apt-get install git -y
 
 if [ ! -f Miniconda-3.6.0-Linux-x86_64.sh ]; then
     wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh
@@ -12,8 +13,12 @@ export PATH=/home/vagrant/miniconda/bin:$PATH
 echo "export PATH=/home/vagrant/miniconda/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 
-chmod +x /vagrant/install.sh
-/vagrant/install.sh
+wget https://raw.githubusercontent.com/kingsgeocomp/install-gis-ubuntu/master/install-gis-mac.sh -O install-gis-mac.sh
+chmod +x ./install-gis-mac.sh
+./install-gis-mac.sh
+
+#chmod +x /vagrant/install.sh
+#/vagrant/install.sh
 
 #wget http://darribas.org/gds15/content/infrastructure/install_gds_stack_unix.sh -O install_gds_stack_unix.sh
 #chmod +x install_gds_stack_unix.sh
